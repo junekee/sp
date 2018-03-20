1715ab51074b222b5a9ab98bf104fee5a0bb66a2
Administrator <june02008@163.com>
2018-03-20 16:26:43 +0800

    2018-3-20  Begin Test

--- src/main/java/com/sp/Begin.java
+++ src/main/java/com/sp/Begin.java
@@ -4,6 +4,7 @@ public class Begin {
     public static void main(String[] args){
 //        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         String fdsfd = "fdsfd";
+
         System.out.println(fdsfd);
     }
 

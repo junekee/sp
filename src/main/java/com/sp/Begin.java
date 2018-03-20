c4c258c0a7f089af53d914d4a9a9e95cae98cce5
Administrator <june02008@163.com>
2018-03-20 16:23:06 +0800

    2018-3-20  Begin

---
+++ src/main/java/com/sp/Begin.java
@@ -0,0 +1,10 @@
+package com.sp;
+
+public class Begin {
+    public static void main(String[] args){
+//        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        String fdsfd = "fdsfd";
+        System.out.println(fdsfd);
+    }
+
+}
